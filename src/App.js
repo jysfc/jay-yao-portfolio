c8134d4c804a64ea44a63240da7743518a555d92
jysfc <63255178+jysfc@users.noreply.github.com>
2020-11-09 21:54:01 -0800

    added projects

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from "react";
 import "./style/master.scss";
+import Project from "./components/Project";
+import { projects } from "./data/projects";
+
+console.log(projects);
 
 function App() {
    return (
       <>
-         <h1 className="text-success">Hello world again</h1>
+         <h1 className="text-danger">Hello world again</h1>
+         <Project />
       </>
    );
 }
---
+++ src/components/Project.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Project() {
+   return (
+      <>
+         <h2 className="text-primary">This is a project</h2>
+      </>
+   );
+}
+
+export default Project;
