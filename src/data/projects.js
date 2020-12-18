2130c8ed60f05b1db5fa29923f23d785ba402c68
jysfc <63255178+jysfc@users.noreply.github.com>
2020-12-17 21:05:25 -0800

    added api redux video

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,29 +1,42 @@
 // name export
 export const projects = [
    {
-      id: `63836685-7a91-454a-aa16-4c92611f88ad`,
-      title: `Validation of Email and Password on Account Creation`,
+      id: "63836685-7a91-454a-aa16-4c92611f88ad",
+      title: "Validation of Email and Password on Account Creation",
       rating: 4,
-      desc: `Validation of email and password fields in White
-         Bear, following best security and UX practices.`,
+      desc:
+         "Validation of email and password fields in White Bear, following best security and UX practices.",
       totalMinutes: 3,
       postedAt: 1604647313142,
-      githubUrl: `https://github.com/jysfc/jay-yao-portfolio`,
-      image: `validation-of-email-and-password-on-account-creation.webp`,
-      youtubeUrl: `//youtu.be/K-1caHSiObc`,
+      githubUrl: "https://github.com/jysfc/jay-yao-portfolio",
+      image: "validation-of-email-and-password-on-account-creation.webp",
+      youtubeUrl: "//youtu.be/K-1caHSiObc",
       isActive: true,
    },
    {
-      id: `12f964a6-b421-49e0-8a58-5d3b9b290a0a`,
-      title: `Walkthrough of Suite Project Prototype`,
+      id: "12f964a6-b421-49e0-8a58-5d3b9b290a0a",
+      title: "Walkthrough of Suite Project Prototype",
       rating: 3,
-      desc: `Prototype of Suite Project, an app that makes finding
-         accommodation easier.`,
+      desc:
+         "Prototype of Suite Project, an app that makes finding accommodation easier.",
       totalMinutes: 4,
       postedAt: 1604647411379,
-      githubUrl: `https://github.com/jysfc/jay-yao-portfolio`,
-      image: `walkthrough-of-suite-project-prototype.webp`,
-      youtubeUrl: `//youtu.be/PTpVOe1NjpQ`,
+      githubUrl: "https://github.com/jysfc/jay-yao-portfolio",
+      image: "walkthrough-of-suite-project-prototype.webp",
+      youtubeUrl: "//youtu.be/PTpVOe1NjpQ",
+      isActive: true,
+   },
+   {
+      id: "cb9f3b9a-c3df-4816-b46c-6eec1cc9af59",
+      title: "Calling data from API and storing in Redux - Suite Project",
+      rating: 5,
+      desc:
+         "Calling data from API and storing it in Redux inside Suite Project",
+      totalMinutes: 4,
+      postedAt: 1608267854073,
+      githubUrl: "https://github.com/jysfc/jay-yao-portfolio",
+      image: "calling-data-from-apiI-and-storing-in-redux -suite-project.webp",
+      youtubeUrl: "//youtu.be/mh4mZox05QM",
       isActive: true,
    },
 ];
